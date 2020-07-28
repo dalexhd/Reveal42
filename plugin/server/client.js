65e90e8ffcef8066ef7ec0e349f9900932d82855
dalexhd <eldalexhd@gmail.com>
2020-07-28 18:04:05 +0200

    test: 1

--- plugin/server/client.js
+++ plugin/server/client.js
@@ -1,7 +1,8 @@
 (function () {
 
 	// don't emit events from inside the previews themselves
-	if (window.location.search.match(/receiver/gi)) { return; }
+
+	if (window.location.search.match(/receiver/gi)) return;
 
 	var socket = io.connect(window.location.origin),
 		socketId = Math.random().toString().slice(2);
@@ -9,20 +10,16 @@
 	console.log('View slide notes at ' + window.location.origin + '/notes/' + socketId);
 
 	let spotifyWindow = null;
-	Reveal.on('slidechanged', (data) => {
-		console.log(data.indexh);
-		if (data.indexh === 0 && spotifyWindow !== null) {
-			return spotifyWindow.close();
-		} else if (data.indexh === 1) {
-			spotifyWindow = window.open('https://spotify-widget.herokuapp.com/player', '_blank', 'width=400,height=200');
-		} else if (data.indexh === 2) {
+
+	const closeSpotifyWindow = function () {
+		if (spotifyWindow !== null) {
 			spotifyWindow.close();
 			spotifyWindow = null;
 		}
-	});
+	}
+
 	window.onbeforeunload = function () {
-		spotifyWindow.close();
-		spotifyWindow = null;
+		closeSpotifyWindow();
 	}
 
 	/**
@@ -50,9 +47,7 @@
 			messageData.notes = notesElement.innerHTML;
 			messageData.markdown = typeof notesElement.getAttribute('data-markdown') === 'string';
 		}
-
 		socket.emit('statechanged', messageData);
-
 	}
 
 	// When a new notes window connects, post our current state
@@ -66,15 +61,18 @@
 	});
 
 	// Monitor events that trigger a change in state
-	Reveal.on('slidechanged', post);
-	Reveal.on('fragmentshown', post);
-	Reveal.on('fragmenthidden', post);
-	Reveal.on('overviewhidden', post);
-	Reveal.on('overviewshown', post);
-	Reveal.on('paused', post);
-	Reveal.on('resumed', post);
+	Reveal.on('slidechanged', (data) => {
+		if (data.indexh === 0 || data.indexh === 2) {
+			return closeSpotifyWindow();
+		} else if (data.indexh === 1 && spotifyWindow !== null) {
+			spotifyWindow = window.open('https://spotify-widget.herokuapp.com/player', '_blank', 'width=400,height=200');
+		}
+		post();
+	});
+	['fragmentshown', 'fragmenthidden', 'overviewhidden', 'overviewshown', 'paused', 'resumed'].forEach((event) => {
+		Reveal.on(event, post);
+	});
 
 	// Post the initial state
 	post();
-
 }());
