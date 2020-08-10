bc7118d59096650952e495e1860556821d66b1cd
dalexhd <eldalexhd@gmail.com>
2020-08-10 09:19:42 +0200

    fix: remove unused tailwind styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-    purge: [],
+    purge: {
+        // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+        enabled: process.env.NODE_ENV === 'production',
+        content: [
+          'index.html'
+        ]
+    },
     theme: {
         extend: {},
     },
     variants: {},
-    plugins: [],
+    plugins: []
 }
