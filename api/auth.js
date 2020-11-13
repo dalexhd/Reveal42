b5de77f89407b893ca5d5f33db11bb11dba9b301
dalexhd <eldalexhd@gmail.com>
2020-11-13 13:53:58 +0100

    feat: Disable ssr and add express session.

--- api/auth.js
+++ api/auth.js
@@ -1,8 +1,64 @@
-const bodyParser = require("body-parser");
-const app = require("express")();
-const axios = require("axios");
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import express from "express";
+import session from "express-session";
+import axios from "axios";
 
+const app = express();
+
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.APP_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Create express router
+const router = express.Router();
+
+// Transform req & res to have the same API as express
+// So we can use res.status() & res.json()
+router.use((req, res, next) => {
+  Object.setPrototypeOf(req, app.request);
+  Object.setPrototypeOf(res, app.response);
+  req.res = res;
+  res.req = req;
+  next();
+});
+
+const getUserData = async (token) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .request({
+        method: "GET",
+        url: "https://api.intra.42.fr/v2/me",
+        headers: {
+          Accept: "application/json",
+          Authorization: token,
+        },
+      })
+      .then(({ data }) => {
+        resolve({
+          id: data.id,
+          email: data.email,
+          login: data.login,
+          first_name: data.first_name,
+          last_name: data.last_name,
+          url: data.url,
+          displayname: data.displayname,
+          image_url: data.image_url,
+          staff: data.staff,
+        });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
 app.post("/intra", (req, res) => {
   axios
     .request({
@@ -22,6 +78,22 @@ app.post("/intra", (req, res) => {
     .catch((error) => {
       res.status(403).send(error);
     });
+});
+
+app.get("/me", async (req, res) => {
+  if (typeof req.session.user === "undefined") {
+    try {
+      req.session.user = await getUserData(req.headers.authorization);
+      req.session.cookie.maxAge = req.cookies["auth._token_expiration.intra"];
+      req.session.save(() => {
+        return res.status(200).send(req.session.user);
+      });
+    } catch (error) {
+      return res.status(401).send(error);
+    }
+  } else {
+    return res.status(200).send(req.session.user);
+  }
 });
 
 module.exports = app;
