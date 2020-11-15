49fa22e244145592dc00b9b7cd184fdda3273100
dalexhd <eldalexhd@gmail.com>
2020-11-15 20:15:28 +0100

    feat: add hotjar-script micropolicy.

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
       {
         hid: "hotjar-script",
         type: "text/x-metomic",
-        "data-micropolicy": "pol:67754a78-980c-49ea-a5e8-3034c77bfc32",
+        "data-micropolicy": "statistics",
         innerHTML: `(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:${process.env.HOTJAR_SITE_ID},hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
       },
       {
